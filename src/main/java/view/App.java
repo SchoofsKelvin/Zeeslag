4cfdb2c43a74c04e10f00fd7855f8f74a80e195e
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2016-11-23 13:30:02 +0100

    Stop asking gridsize

--- src/main/java/view/App.java
+++ src/main/java/view/App.java
@@ -8,8 +8,6 @@ public class App {
 	public static void main(String[] args) {
 		GridsizeMenu menu = new GridsizeMenu();
 		String name = menu.askPlayerName();
-		int size = menu.getGridSize();
-		// BoardFrame board = new BoardFrame(size,name);
 		BattleshipGame game = new BattleshipGame(new Player(name));
 	}
 
