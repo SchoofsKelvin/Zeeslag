2af37e9695de6d6439c3abce5ba8d406e72be106
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2016-11-25 15:16:58 +0100

    Add color stuff to BattleshipBoardCell

--- src/main/java/view/battleship/BattleshipBoardCell.java
+++ src/main/java/view/battleship/BattleshipBoardCell.java
@@ -1,5 +1,6 @@
 package view.battleship;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -10,33 +11,51 @@ import view.BoardCell;
 
 public class BattleshipBoardCell extends BoardCell implements ActionListener {
 
-	private static final long serialVersionUID = -3288098740775513486L;
-	private static final ImageIcon X = new ImageIcon("images/x.png");
-	private static final ImageIcon O = new ImageIcon("images/o.png");
+	private static final long		serialVersionUID	= -3288098740775513486L;
+	private static final ImageIcon	X					= new ImageIcon("images/x.png");
+	private static final ImageIcon	O					= new ImageIcon("images/o.png");
 
-	private int a = 0;
+	private int						a					= 0;
 
-	private final BattleshipGame game;
+	private final BattleshipGame	game;
 
 	public BattleshipBoardCell(BattleshipGame game, int size) {
 		super(size);
 		this.game = game;
 		addActionListener(this);
+		setColor(CellColor.Empty);
 	}
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		a = ++a % 3;
 		switch (a) {
-		case 0:
-			setIcon(null);
-			break;
-		case 1:
-			setIcon(X);
-			break;
-		case 2:
-			setIcon(O);
-			break;
+			case 0:
+				// setIcon(null);
+				setBackground(Color.GRAY);
+				break;
+			case 1:
+				// setIcon(X);
+				setBackground(Color.GREEN);
+				break;
+			case 2:
+				// setIcon(O);
+				setBackground(Color.WHITE);
+				break;
+		}
+	}
+
+	public void setColor(CellColor c) {
+		setBackground(c.color);
+	}
+
+	public enum CellColor {
+		Empty(Color.GRAY), Ship(Color.WHITE), Hit(Color.RED), Shot(Color.DARK_GRAY);
+
+		public final Color color;
+
+		CellColor(Color col) {
+			this.color = col;
 		}
 	}
 
