215f66f8833c3cd58518bce67439d42cc9a2e966
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2016-11-25 16:13:07 +0100

    Create Board and Cell classes in the model package

---
+++ src/main/java/model/Board.java
@@ -0,0 +1,26 @@
+package model;
+
+import exception.DomainException;
+
+public abstract class Board {
+
+	private Cell[][] cells;
+
+	public void resetBoard(int gridSize) {
+		cells = new Cell[gridSize][gridSize];
+		for (int x = 0; x < gridSize; x++) {
+			for (int y = 0; y < gridSize; y++) {
+				cells[x][y] = createCell(x, y);
+			}
+		}
+	}
+
+	protected abstract Cell createCell(int x, int y);
+
+	public Cell getCell(int x, int y) throws DomainException {
+		if (x < 0 || x >= cells.length) throw new DomainException("Invalid X");
+		if (y < 0 || y >= cells.length) throw new DomainException("Invalid Y");
+		return cells[x][y];
+	}
+
+}
