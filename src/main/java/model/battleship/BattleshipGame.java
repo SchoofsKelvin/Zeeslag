f4e2d002ba1bdc9ada4ec558eed9428eac8c822d
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2016-11-25 15:26:39 +0100

    Update BattleshipGame and BattleshipBoardCell

--- src/main/java/model/battleship/BattleshipGame.java
+++ src/main/java/model/battleship/BattleshipGame.java
@@ -2,24 +2,18 @@ package model.battleship;
 
 import exception.DomainException;
 import model.Player;
-import view.BoardCell;
-import view.BoardCellFactory;
 import view.BoardFrame;
 import view.battleship.BattleshipBoardCell;
 
 public class BattleshipGame {
 
-	private Player player1, player2;
+	private Player		player1, player2;
 
-	private BoardFrame frame;
+	private BoardFrame	frame;
 
 	public BattleshipGame(Player player) throws DomainException {
 		this.player1 = player;
 		this.player2 = new AI();
-		this.BattleshipGames();
-	}
-
-	public void BattleshipGames() {
 		frame = new BoardFrame(10, buttonsize -> new BattleshipBoardCell(this, buttonsize));
 		frame.setLeftName(player1.getName());
 		frame.setRightName(player2.getName());
@@ -27,7 +21,6 @@ public class BattleshipGame {
 
 	/*
 	 * frame = new BoardFrame(10, new BoardCellFactory() {
-	 * 
 	 * @Override public BoardCell createCell(int buttonsize) { return new
 	 * BattleshipBoardCell(BattleshipGame.this, buttonsize); } });
 	 */
--- src/main/java/view/battleship/BattleshipBoardCell.java
+++ src/main/java/view/battleship/BattleshipBoardCell.java
@@ -12,12 +12,12 @@ import view.BoardCell;
 public class BattleshipBoardCell extends BoardCell implements ActionListener {
 
 	private static final long		serialVersionUID	= -3288098740775513486L;
-	private static final ImageIcon	X					= new ImageIcon("images/x.png");
-	private static final ImageIcon	O					= new ImageIcon("images/o.png");
+	public static final ImageIcon	X					= new ImageIcon("images/x.png");
+	public static final ImageIcon	O					= new ImageIcon("images/o.png");
 
 	private int						a					= 0;
 
-	private final BattleshipGame	game;
+	public final BattleshipGame		game;
 
 	public BattleshipBoardCell(BattleshipGame game, int size) {
 		super(size);
@@ -28,19 +28,19 @@ public class BattleshipBoardCell extends BoardCell implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		a = ++a % 3;
+		a = ++a % 4;
 		switch (a) {
 			case 0:
-				// setIcon(null);
-				setBackground(Color.GRAY);
+				setColor(CellColor.Empty);
 				break;
 			case 1:
-				// setIcon(X);
-				setBackground(Color.GREEN);
+				setColor(CellColor.Ship);
 				break;
 			case 2:
-				// setIcon(O);
-				setBackground(Color.WHITE);
+				setColor(CellColor.Hit);
+				break;
+			case 3:
+				setColor(CellColor.Shot);
 				break;
 		}
 	}
@@ -50,7 +50,7 @@ public class BattleshipBoardCell extends BoardCell implements ActionListener {
 	}
 
 	public enum CellColor {
-		Empty(Color.GRAY), Ship(Color.WHITE), Hit(Color.RED), Shot(Color.DARK_GRAY);
+		Empty(new Color(100, 197, 255)), Ship(Color.WHITE), Hit(Color.RED), Shot(Color.GRAY);
 
 		public final Color color;
 
