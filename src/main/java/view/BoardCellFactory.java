11dd2dfeac7918b919cd2834e655829c8d138ba4
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2016-11-25 12:56:18 +0100

    Explanation

--- src/main/java/view/BoardCellFactory.java
+++ src/main/java/view/BoardCellFactory.java
@@ -5,5 +5,10 @@ public interface BoardCellFactory {
 	public BoardCell createCell(int buttonsize);
 
 	public final BoardCellFactory standard = buttonsize -> new BoardCell(buttonsize);
+	// public final BoardCellFactory standard = new BoardCellFactory() {
+	// public BoardCell createCell(int buttonsize) {
+	// return new BoardCell(buttonsize);
+	// };
+	// }
 
 }
