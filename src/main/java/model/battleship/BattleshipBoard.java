1f9dc95972af28a9ffb23c1991c2edead06b05ec
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2016-11-25 16:57:37 +0100

    Solve conflict

--- src/main/java/model/battleship/BattleshipBoard.java
+++ src/main/java/model/battleship/BattleshipBoard.java
@@ -3,8 +3,18 @@ package model.battleship;
 import exception.DomainException;
 import model.Board;
 import model.Cell;
+import model.Player;
+import model.battleship.BattleshipGame.Turn;
 
 public class BattleshipBoard extends Board {
+
+	public final BattleshipGame	game;
+	public final Player			player;
+
+	public BattleshipBoard(BattleshipGame game, Player player) {
+		this.game = game;
+		this.player = player;
+	}
 
 	@Override
 	protected Cell createCell(int x, int y) {
@@ -14,6 +24,18 @@ public class BattleshipBoard extends Board {
 	@Override
 	public BattleshipCell getCell(int x, int y) throws DomainException {
 		return (BattleshipCell) super.getCell(x, y);
+	}
+
+	public void clickedCell(int x, int y, boolean other) throws DomainException {
+		Turn turn = game.getTurn();
+		if (turn == Turn.Starting && other) return;
+		if (turn != Turn.Starting && game.getActivePlayer() != player) return;
+		BattleshipCell cell = getCell(x, y);
+		if (other) {
+			game.shoot(x, y);
+		} else if (turn == Turn.Starting) {
+			game.placeBoat(player, cell);
+		}
 	}
 
 }
