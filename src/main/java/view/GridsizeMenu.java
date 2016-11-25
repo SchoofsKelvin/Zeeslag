3b7a03eda6187f126b43ffc111adede8e3d7dc95
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2016-11-25 16:07:43 +0100

    Fix NullPointerException on Cancel/Close

--- src/main/java/view/GridsizeMenu.java
+++ src/main/java/view/GridsizeMenu.java
@@ -1,29 +1,30 @@
 package view;
 
-
 import javax.swing.JOptionPane;
 
 public class GridsizeMenu {
+
 	private String[] gridSize = { "5", "9", "10", "20" };
 
-	public GridsizeMenu() {
-	}
+	public GridsizeMenu() {}
 
 	public int getGridSize() {
-		String size = (String) JOptionPane.showInputDialog(null, "On which grid size, do you want to play?",
-				"Choose the Grid Size", JOptionPane.QUESTION_MESSAGE, null, gridSize, gridSize[2]);
+		String size = (String) JOptionPane.showInputDialog(null,
+			"On which grid size, do you want to play?", "Choose the Grid Size",
+			JOptionPane.QUESTION_MESSAGE, null, gridSize, gridSize[2]);
 		return Integer.parseInt(size);
 	}
 
 	public String askPlayerName() {
 		String name = "";
-		while (name.trim().isEmpty() || name.length() < 3||name.length()>9) {
+		while (name == null || name.trim().isEmpty() || name.length() < 3
+			|| name.length() > 9) {
 			name = JOptionPane.showInputDialog(null, "What is your name?");
 		}
 		return name;
 	}
 
 	/**
-	 *  this class is used to create a scroll menu!
+	 * this class is used to create a scroll menu!
 	 */
 }
