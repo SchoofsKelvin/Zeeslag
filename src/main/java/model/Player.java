8766ec7654470698a113f961bf6ab7c466b2f08e
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2016-12-01 16:56:31 +0100

    Add toString() to Player

--- src/main/java/model/Player.java
+++ src/main/java/model/Player.java
@@ -13,11 +13,16 @@ public class Player {
 	public String getName() {
 		return name;
 	}
-	private void setName(String name) throws DomainException{
-		if(name==null||name.trim().isEmpty()){
-			throw new DomainException("The name may not be empty or null");
-		}
+
+	private void setName(String name) throws DomainException {
+		if (name == null || name.trim()
+			.isEmpty()) { throw new DomainException("The name may not be empty or null"); }
 		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Player(\"" + name + "\"";
 	}
 
 }
