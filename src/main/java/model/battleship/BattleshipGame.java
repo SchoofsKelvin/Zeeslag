f7649f15b9a106513bb65245caa4bd6586b373f2
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2016-12-02 16:46:58 +0100

    Finish FollowUpStrategy

--- src/main/java/model/battleship/BattleshipGame.java
+++ src/main/java/model/battleship/BattleshipGame.java
@@ -51,7 +51,6 @@ public class BattleshipGame {
 	public boolean shoot(int x, int y) throws DomainException {
 		if (x < 0 || x >= gridSize) return false;
 		if (y < 0 || y >= gridSize) return false;
-		System.out.println("Shoot (" + x + ", " + y + ")");
 		if (turn == TurnState.Starting) return false;
 		boolean leTurn = turn == TurnState.Player1;
 		BattleshipBoard board = leTurn ? board2 : board1;
