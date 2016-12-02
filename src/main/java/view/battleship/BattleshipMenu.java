cb6fd8a3ac4e7fd600434e75d099c28c3c8b5d60
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2016-12-02 16:10:51 +0100

    Added FollowUpStrategy (not implemented yet) Removed unnecessary toString()s from Strategy classes Moved view.BattleshipMenu to view.battleship.BattleshipMenu

--- src/main/java/view/battleship/BattleshipMenu.java
+++ src/main/java/view/battleship/BattleshipMenu.java
@@ -1,19 +1,25 @@
-package view;
+package view.battleship;
+
+import java.util.ArrayList;
 
 import javax.swing.JOptionPane;
 
 import model.battleship.ai.AllSeeingEnemyStrategy;
+import model.battleship.ai.FollowUpStrategy;
 import model.battleship.ai.RandomStrategy;
 import model.battleship.ai.Strategy;
 import model.battleship.ai.Strategy.StrategyFactory;
 
 public class BattleshipMenu {
 
-	private final static String[]			gridSize	= { "5", "9", "10", "20" };
-	private final static StrategyFactory[]	strategies	=
-		{ RandomStrategy.factory, AllSeeingEnemyStrategy.factory };
+	private final static String[]				gridSize	= { "5", "9", "10", "20" };
+	private final ArrayList<StrategyFactory>	strategies	= new ArrayList<>();
 
-	public BattleshipMenu() {}
+	public BattleshipMenu() {
+		addStrategyFactory(RandomStrategy.factory);
+		addStrategyFactory(FollowUpStrategy.factory);
+		addStrategyFactory(AllSeeingEnemyStrategy.factory);
+	}
 
 	public int getGridSize() {
 		String size = (String) JOptionPane.showInputDialog(null,
@@ -34,7 +40,11 @@ public class BattleshipMenu {
 	public Strategy askStrategy() {
 		StrategyFactory fac = (StrategyFactory) JOptionPane.showInputDialog(null,
 			"Which strategy for the AI do you want to use?", "Choose an AI Strategy",
-			JOptionPane.QUESTION_MESSAGE, null, strategies, strategies[0]);
+			JOptionPane.QUESTION_MESSAGE, null, strategies.toArray(), strategies.get(0));
 		return fac == null ? null : fac.create();
+	}
+
+	public void addStrategyFactory(StrategyFactory factory) {
+		strategies.add(factory);
 	}
 }
