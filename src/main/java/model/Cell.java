6bc7194e5f226746705e409c409811dbe4eb8239
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2016-12-02 15:15:13 +0100

    Undo weird stuff with Cell

--- src/main/java/model/Cell.java
+++ src/main/java/model/Cell.java
@@ -1,42 +1,19 @@
 package model;
 
-import exception.DomainException;
-
 public class Cell {
 
-	public int x, y;
+	public final int x, y;
 
 	public Cell(int x, int y) {
-		setX(x);
-		setY(y);
-	}
-
-	public int getX() {
-		return x;
-	}
-
-	public int getY() {
-		return y;
-	}
-
-	private void setX(int x) {
-		
 		this.x = x;
-	}
-	
-	@Override
-	public boolean equals(Object cell){
-		if(!(cell instanceof Cell)){
-			return false;
-		}
-		Cell c = (Cell) cell;
-		return this.x == c.x && this.y == c.y;
+		this.y = y;
 	}
 
-	private void setY(int y) {
-		
-		this.y=y;
+	@Override
+	public boolean equals(Object cell) {
+		if ( !(cell instanceof Cell)) return false;
+		Cell c = (Cell) cell;
+		return x == c.x && y == c.y;
 	}
-	
 
 }
