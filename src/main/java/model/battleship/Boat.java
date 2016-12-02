1a11703abd6836a4372660ec82cfc64734181904
Kelvin Schoofs <schoofs.kelvin@gmail.com>
2016-12-02 12:33:40 +0100

    Finished Story 3 Allow placing same type of ship several times

--- src/main/java/model/battleship/Boat.java
+++ src/main/java/model/battleship/Boat.java
@@ -2,18 +2,20 @@ package model.battleship;
 
 public enum Boat {
 
-	AircraftCarrier(5, "Aircraft Carrier"),
-	Battleship(4, "Battleship"),
-	Submarine(3, "Submarine"),
-	Destroyer(3, "Destroyer"),
-	PatrolShip(2, "Patrol Ship");
+	AircraftCarrier(5, "Aircraft Carrier", 1),
+	Battleship(4, "Battleship", 2),
+	Submarine(3, "Submarine", 3),
+	Destroyer(3, "Destroyer", 3),
+	PatrolShip(2, "Patrol Ship", 4);
 
 	public final int	length;
 	public final String	fullname;
+	public final int	amount;
 
-	Boat(int length, String fullname) {
+	Boat(int length, String fullname, int amount) {
 		this.length = length;
 		this.fullname = fullname;
+		this.amount = amount;
 	}
 
 	@Override
